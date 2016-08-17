c5e8665d421045b3ad43e34632ea9e7fcf653bf2
Marie Denney <mldenne@gmail.com>
2016-08-17 11:08:43 -0400

    Updated model calculations and views

--- app/models/commute.rb
+++ app/models/commute.rb
@@ -16,8 +16,16 @@ class Commute < ApplicationRecord
     (drive_minutes_per_week / 60.0).round(2)
   end
 
+  def drive_days_per_year
+    ((drive_hours_per_week * 50) / 24.0).round(2)
+  end
+
   def drive_cost_per_week
     (distance_per_week * 0.54).round(0)
+  end
+
+  def drive_cost_per_year
+    (drive_cost_per_week * 50).round(0)
   end
 
 end
