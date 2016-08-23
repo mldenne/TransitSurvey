13a00fa3a6e8e8f64dcb8f74325e34e66c3eb5b7
Marie Denney <mldenne@gmail.com>
2016-08-23 16:21:40 -0400

    Added cities to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+f = File.open("IndyDMA.txt")
+city = f.read.split("\n").sort
