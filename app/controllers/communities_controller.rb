61478a035a41ae646d59491e4d079b68415303f4
Marie Denney <mldenne@gmail.com>
2016-08-24 12:04:54 -0400

    Added heatmap for communities

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,11 +1,13 @@
 class CommunitiesController < ApplicationController
 
   def index
-    @community.Community.all
+    @community = Community.all
   end
 
   def show
-    @community.Community.find(params[:id])
+    @community = Community.find(params[:id])
+    commute_ids = @community.commuters_within_range.ids
+    @points = Point.where(commute_id: commute_ids)
   end
 
 end
--- app/models/community.rb
+++ app/models/community.rb
@@ -3,7 +3,7 @@ class Community < ApplicationRecord
   acts_as_mappable :auto_geocode=>{:field=>:city, :error_message=>'Could not geocode address'}
 
   def commuters_within_range
-    Commute.within(self.range, :origin => [self.lat, self.lng])
+    Commute.within(range, :origin => [lat, lng])
   end
 
 end
