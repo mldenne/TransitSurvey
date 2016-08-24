63a422acc5b77892fb896b86e2ce0f1291aa36d6
Marie Denney <mldenne@gmail.com>
2016-08-24 10:12:17 -0400

    Updated view styling, added comments to code in models

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,4 +2,8 @@ class Community < ApplicationRecord
 
   acts_as_mappable :auto_geocode=>{:field=>:city, :error_message=>'Could not geocode address'}
 
+  def commuters_within_range
+    Commute.within(self.range, :origin => [self.lat, self.lng])
+  end
+
 end
