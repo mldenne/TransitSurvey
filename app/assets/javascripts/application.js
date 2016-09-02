4c619e5cd21653dc74c160f486a92e4698a0b389
Marie Denney <mldenne@gmail.com>
2016-09-02 15:36:12 -0400

    Added loading gif, added Google API server key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+  $('#loading-indicator').hide();  // hide it initially.
+  $(document)
+    .ajaxStart(function() {
+      $('#loading-indicator').show(); // show on any Ajax event.
+    })
+    .ajaxStop(function() {
+      $('#loading-indicator').hide(); // hide it when it is done.
+  });
+});
